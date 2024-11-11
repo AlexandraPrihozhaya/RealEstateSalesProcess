4da8649c7819c499b964f00cada01836d5badb2c
Alexandra <prihsasha@mail.ru>
2024-11-11 03:17:14 +0300

    11.11.2024

--- realestateagencyclient/src/components/blockDescriptionComponents/BlockAboutUs/index.js
+++ realestateagencyclient/src/components/blockDescriptionComponents/BlockAboutUs/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { SSection } from './styled';
-import AboutUsPhoto from '../AboutUsPhoto';
-import AboutUsText from '../AboutUsText';
+import { SSection, SDivMain, SDiv1, SDiv2, SDiv3, SDivText, SH1, SText, SFaHandPointRight } from './styled';
+
 
 
 const BlockAboutUs = () => {
   return (
     <SSection>
-      <AboutUsText />
-      <AboutUsPhoto />
-    </SSection>
+    <SDivMain>
+        <SDiv1>
+            <SDivText>
+                <SH1>Адрес Успеха</SH1>
+            </SDivText>
+        </SDiv1>
+        <SDiv2>
+           <SDivText>
+                <SText>
+                    Адрес Успеха - это команда опытных и заботливых риэлторов, которые помогут вам реализовать мечту о собственном доме. Мы знаем рынок недвижимости как свои пять пальцев, и всегда готовы предложить вам наилучшие варианты по выгодной цене.
+                </SText>
+                <SText>
+                    Мы оказываем полный спектр услуг:
+                </SText>
+                <SText>
+                    <SFaHandPointRight /> Поиск и подбор недвижимости. Узнайте о ваших мечтах и предпочтениях, и мы подберем идеальный вариант, чтобы вы могли наслаждаться своей новой жизнью.
+                </SText>
+                <SText>
+                    <SFaHandPointRight /> Юридическое сопровождение. Наши юристы проверят недвижимость на юридическую чистоту и обеспечат безопасность сделки.
+                </SText>
+                <SText>
+                    <SFaHandPointRight /> Переговоры с продавцами. Мы ведем переговоры от вашего имени, чтобы вы получили лучшую цену и выгодные условия.
+                </SText>
+                <SText>
+                    <SFaHandPointRight /> Оформление документов. Сбор и подготовка всех необходимых документов для быстрой и успешной сделки.
+                </SText>
+            </SDivText>     
+        </SDiv2>
+            <SDiv3></SDiv3>
+        </SDivMain>
+        </SSection>
   );
 };
 
--- realestateagencyclient/src/components/blockDescriptionComponents/BlockAboutUs/styled.js
+++ realestateagencyclient/src/components/blockDescriptionComponents/BlockAboutUs/styled.js
@@ -1,12 +1,52 @@
 import styled from 'styled-components';
+import { FaHandPointRight } from "react-icons/fa";
+import photo from "D:/Документы/БГУИР/8 сем/дипломный проект/RealEstateSalesProcess/realestateagencyclient/src/assets/aboutcompany.jpg"
 
 export const SSection = styled.section`
-  background-color: #ffffff;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  margin: 40px 200px;
-  height: 600px;
+  padding: 40px 200px;
+`;
+
+export const SDivMain = styled.div`
   box-shadow: 0 4px 8px rgba(72, 72, 72, 0.3);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+`;
+
+export const SDiv1 = styled.div`
+  background-color: aliceblue;
+  background: #484848;
+`;
+
+export const SDiv2 = styled.div`
+  grid-column-start: 1;
+  grid-row-start: 2;
+`;
+
+export const SDiv3 = styled.div`
+  grid-row: span 2 / span 2;
+  grid-column-start: 2;
+  grid-row-start: 1;
+  background-image: url(${photo});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export const SH1 = styled.h1`
+    color: #fff;
+    font-size: 38px;
+    line-height: 1;
+`;
+
+export const SDivText = styled.div`
+    margin: 30px;
+`;
+
+export const SText = styled.p`
+    color: #000;
+    font-style: italic;
+`;
+
+export const SFaHandPointRight = styled(FaHandPointRight)`
+    color: #ff8a00;
 `;
